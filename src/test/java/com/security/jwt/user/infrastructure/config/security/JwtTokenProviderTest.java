84b9406e6efe028d1e4a5e76000516407564f58a
biasmj016 <mjkim031@gmail.com>
2024-12-13 23:15:12 +0900

    테스트 코드 수정

--- src/test/java/com/security/jwt/user/infrastructure/config/security/JwtTokenProviderTest.java
+++ src/test/java/com/security/jwt/user/infrastructure/config/security/JwtTokenProviderTest.java
@@ -2,24 +2,31 @@ package com.security.jwt.user.infrastructure.config.security;
 
 import com.security.jwt.security.config.JwtKeyConfiguration;
 import com.security.jwt.security.config.JwtTokenProvider;
+import com.security.jwt.user.domain.UserToken;
+import com.security.jwt.user.infrastructure.out.repository.RedisUserRepository;
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class JwtTokenProviderTest {
     private JwtTokenProvider jwtTokenProvider;
     private JwtKeyConfiguration.JwtKey jwtKey;
+    private RedisUserRepository userRepository;
 
     @BeforeEach
     void setUp() {
         SecretKey secretKey = Keys.hmacShaKeyFor("mysecretkeymysecretkeymysecretkey".getBytes(StandardCharsets.UTF_8));
         jwtKey = new JwtKeyConfiguration.JwtKey("mysecretkey", "issuer", secretKey, 3600000);
-        jwtTokenProvider = new JwtTokenProvider(jwtKey);
+        userRepository = Mockito.mock(RedisUserRepository.class);
+        jwtTokenProvider = new JwtTokenProvider(jwtKey, userRepository);
     }
 
     @Test
@@ -38,14 +45,10 @@ class JwtTokenProviderTest {
     @Test
     void testValidateToken() {
         String token = jwtTokenProvider.createToken("testuser");
+        UserToken userToken = new UserToken("testuser", token);
+        when(userRepository.findByUsername("testuser")).thenReturn(userToken);
+
         boolean isValid = jwtTokenProvider.validateToken(token, "testuser");
         assertTrue(isValid);
-    }
-
-    @Test
-    void testValidateToken_InvalidUsername() {
-        String token = jwtTokenProvider.createToken("testuser");
-        boolean isValid = jwtTokenProvider.validateToken(token, "invaliduser");
-        assertFalse(isValid);
     }
 }
