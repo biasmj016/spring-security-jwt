f50dde24f8f224d3caaf6335cd180c18090bab23
biasmj016 <mjkim031@gmail.com>
2024-12-13 23:10:27 +0900

    토큰 검증 코드 변경

--- src/main/java/com/security/jwt/user/domain/UserToken.java
+++ src/main/java/com/security/jwt/user/domain/UserToken.java
@@ -1,3 +1,8 @@
 package com.security.jwt.user.domain;
 
-public record UserToken(String username, String token) {}
+public record UserToken(String username, String token) {
+    public boolean isTokenValid(String token) {
+        if (token == null) return false;
+        return this.token.equals(token);
+    }
+}
